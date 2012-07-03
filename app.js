b25cbc98aef0b300541259a8b360cac9dabd5c0a
Markus Schleehauf <schleehauf.markus@gmail.com>
2012-07-03 23:45:55 +0200

    added history support

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var Express = require('express');
 var parseCookie = require('./lib/utils').parseCookie;
 var sessionStore = new Express.session.MemoryStore();
+var redis = require("redis");
 var PORT = 8080;
 
 /*
@@ -13,6 +14,8 @@ var PORT = 8080;
  */
 var app = module.exports = Express.createServer();
 var io = require('socket.io').listen(app);
+var rclient = redis.createClient();
+
 
 app.configure(function () {
     app.set('views', __dirname + '/views');
@@ -24,14 +27,37 @@ app.configure(function () {
     app.use(Express['static'](__dirname + '/public'));
 });
 
+
+
 io.sockets.on('connection', function(socket) {
     var session = socket.handshake.session;
     socket.broadcast.emit('new_user', session.username);
+    
+    var history = [];
+    
+    rclient.llen("messages", function(err, history_len){
+	    console.log("history: " + history_len);
+	    rclient.lrange("messages", 0, history_len, function(err, rhistory){
+		    //socket.broadcast(rhistory);
+		    //console.log(rhistory);
+		    //history = JSON.parse(rhistory);
+		    rhistory.forEach(function(message, i){
+				io.sockets.emit('broadcast', JSON.parse(message));
+		    })
+
+	    })
+	    
+    });
+    
+    
     socket.join(session.username);
 
     socket.on('broadcast', function(message){
         message.sender = session.username;
         io.sockets.emit('broadcast', message);
+        //TODO: Write to redis
+        rclient.rpush("messages", JSON.stringify(message));
+        
     });
 
     socket.on('whisper', function(message){
