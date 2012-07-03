dbd2bcf8f251a1d50c2bd7d1e1a155dfa8c6f519
Raphael Brand <raphaelbrand@gmail.com>
2012-07-03 09:56:36 +0200

    authentification

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var Express = require('express');
-
+var parseCookie = require('./lib/utils').parseCookie;
+var sessionStore = new Express.session.MemoryStore();
 var PORT = 8080;
 
 /*
@@ -16,30 +17,61 @@ var io = require('socket.io').listen(app);
 app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.use(Express.bodyParser());
     app.use(Express.cookieParser());
-    app.use(Express.session( {secret: "mySecret"} ));
+    app.use(Express.session( {store: sessionStore, secret: "mySecret"} ));
     app.use(app.router);
     app.use(Express['static'](__dirname + '/public'));
 });
 
 io.sockets.on('connection', function(socket) {
-    socket.on('join', function(username) {
-        socket.broadcast.emit('new_user', username);
-        socket.join(username);
-    });
+    var session = socket.handshake.session;
+    socket.broadcast.emit('new_user', session.username);
+    socket.join(session.username);
 
     socket.on('broadcast', function(message){
+        message.sender = session.username;
         io.sockets.emit('broadcast', message);
     });
 
     socket.on('whisper', function(message){
+        message.sender = session.username;
         io.sockets.in(message.reciever).emit('whisper', message);
     });
 });
 
+io.set('authorization', function (handshake, accept) {
+    handshake.cookie = parseCookie(handshake.headers.cookie);
+    handshake.sessionID = handshake.cookie['connect.sid'];
+    sessionStore.get(handshake.sessionID, function (err, session) {
+      if (err || !session) {
+        accept('Error', false);
+      } else {
+        handshake.session = session;
+        accept(null, true);
+      }
+    });
+});
 
-app.get('/', function(req, res, next) {
+auth = function(req, res, next) {
+  if (req.session.username != null) {
+    next();
+  } else {
+    res.redirect('/auth');
+  }
+};
+
+app.get('/', auth, function(req, res, next) {
     res.render('chat');
+});
+
+app.get('/auth', function(req, res, next) {
+    res.render('auth');
+});
+
+app.post('/auth', function(req, res, next) {
+    req.session.username = req.body.username;
+    res.redirect('/');
 });
 
 
--- public/chat_client.js
+++ public/chat_client.js
@@ -1,8 +1,18 @@
 $(function(){
-    var socket;
-    var username;
+    var socket = io.connect('http://localhost');
 
-    console.log('START');
+    socket.on('new_user', function(username) {
+        appendToTextarea('Say Hello to ' + username);
+      });
+
+    socket.on('broadcast', function(message) {
+        appendToTextarea(message.sender + ': ' + message.text);
+      });
+
+    socket.on('whisper', function(message) {
+        appendToTextarea(message.sender + ' whispers: ' + message.text);
+      });
+
 
     function isPrivateMessage(text) {
       return text.split(' ')[0] === '/msg';
@@ -10,12 +20,14 @@ $(function(){
 
     function sendText() {
       var message2send = $("#input").val();
+      console.log(message2send);
       if(isPrivateMessage(message2send)){
+        console.log("private message");
         var text = message2send.split(' ');
-        socket.emit('whisper', {sender: username, reciever: text[1], text: text[2]});
+        socket.emit('whisper', {reciever: text[1], text: text[2]});
       }
       else{
-        socket.emit('broadcast', {sender: username, text: message2send});
+        socket.emit('broadcast', {text: message2send});
       }
       $("#input").val('');
     }
@@ -25,29 +37,6 @@ $(function(){
       $("#chatlog").val(chatlog);
     }
 
-
-    $("#connect").click(function(){
-        socket = io.connect('http://localhost');
-        username = $("#username").val();
-
-        socket.on('connect', function(data) {
-            socket.emit('join', username);
-        });
-
-        socket.on('new_user', function(username) {
-            appendToTextarea('Say Hello to ' + username);
-        });
-
-        socket.on('broadcast', function(message) {
-            appendToTextarea(message.sender + ': ' + message.text);
-        });
-
-        socket.on('whisper', function(message) {
-            appendToTextarea(message.sender + ' whispers: ' + message.text);
-        });
-
-      });
-
     $("#send").click(function(){
         sendText();
       })
@@ -55,7 +44,7 @@ $(function(){
     $(document).keydown(function (e) {
         if (/^(input|textarea)$/i.test(e.target.nodeName) || e.target.isContentEditable) {
           if (e.keyCode === 13) {
-            sendText()
+            sendText();
           }
           return;
         }
